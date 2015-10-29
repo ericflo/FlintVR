8d42af861fa8a5ab89562aaf25564dcde5954a96
Eric Florenzano <floguy@gmail.com>
2015-10-29 16:32:05 -0700

    Modernize Vector4

--- Src/CoreVector4f.cpp
+++ Src/CoreVector4f.cpp
@@ -9,23 +9,113 @@ static JSClass coreVector4fClass = {
   JSCLASS_HAS_PRIVATE,    /* flags */
   NULL,
   NULL,
-  CoreVector4f_getProperty,
-  CoreVector4f_setProperty,
+  NULL,
+  NULL,
   NULL,
   NULL,
   NULL,
   CoreVector4f_finalize
 };
 
-JSObject* NewCoreVector4f(JSContext* cx, OVR::Vector4f* vector4f) {
+static bool CoreVector4f_get_x(JSContext* cx, unsigned argc, JS::Value *vp) {
+  JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
+  JS::RootedObject self(cx, &args.thisv().toObject());
+  OVR::Vector4f* item = GetVector4f(self);
+  args.rval().setNumber(item->x);
+  return true;
+}
+
+static bool CoreVector4f_set_x(JSContext* cx, unsigned argc, JS::Value* vp) { \
+  JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
+  if (!args[0].isNumber()) {
+    JS_ReportError(cx, "Invalid number specified");
+    return false;
+  }
+  JS::RootedObject self(cx, &args.thisv().toObject());
+  OVR::Vector4f* item = GetVector4f(self);
+  item->x = args[0].toNumber();
+  return true;
+}
+
+static bool CoreVector4f_get_y(JSContext* cx, unsigned argc, JS::Value *vp) {
+  JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
+  JS::RootedObject self(cx, &args.thisv().toObject());
+  OVR::Vector4f* item = GetVector4f(self);
+  args.rval().setNumber(item->y);
+  return true;
+}
+
+static bool CoreVector4f_set_y(JSContext* cx, unsigned argc, JS::Value* vp) { \
+  JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
+  if (!args[0].isNumber()) {
+    JS_ReportError(cx, "Invalid number specified");
+    return false;
+  }
+  JS::RootedObject self(cx, &args.thisv().toObject());
+  OVR::Vector4f* item = GetVector4f(self);
+  item->y = args[0].toNumber();
+  return true;
+}
+
+static bool CoreVector4f_get_z(JSContext* cx, unsigned argc, JS::Value *vp) {
+  JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
+  JS::RootedObject self(cx, &args.thisv().toObject());
+  OVR::Vector4f* item = GetVector4f(self);
+  args.rval().setNumber(item->z);
+  return true;
+}
+
+static bool CoreVector4f_set_z(JSContext* cx, unsigned argc, JS::Value* vp) { \
+  JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
+  if (!args[0].isNumber()) {
+    JS_ReportError(cx, "Invalid number specified");
+    return false;
+  }
+  JS::RootedObject self(cx, &args.thisv().toObject());
+  OVR::Vector4f* item = GetVector4f(self);
+  item->z = args[0].toNumber();
+  return true;
+}
+
+static bool CoreVector4f_get_w(JSContext* cx, unsigned argc, JS::Value *vp) {
+  JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
+  JS::RootedObject self(cx, &args.thisv().toObject());
+  OVR::Vector4f* item = GetVector4f(self);
+  args.rval().setNumber(item->w);
+  return true;
+}
+
+static bool CoreVector4f_set_w(JSContext* cx, unsigned argc, JS::Value* vp) { \
+  JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
+  if (!args[0].isNumber()) {
+    JS_ReportError(cx, "Invalid number specified");
+    return false;
+  }
+  JS::RootedObject self(cx, &args.thisv().toObject());
+  OVR::Vector4f* item = GetVector4f(self);
+  item->w = args[0].toNumber();
+  return true;
+}
+
+static JSPropertySpec CoreVector4f_props[] = {
+  VRJS_PROP(CoreVector4f, x),
+  VRJS_PROP(CoreVector4f, y),
+  VRJS_PROP(CoreVector4f, z),
+  VRJS_PROP(CoreVector4f, w),
+  JS_PS_END
+};
+
+JSObject* NewCoreVector4f(JSContext* cx, OVR::Vector4f* vec) {
 	JS::RootedObject self(cx, JS_NewObject(cx, &coreVector4fClass));
-	JS_SetPrivate(self, (void *)vector4f);
+	if (!JS_DefineProperties(cx, self, CoreVector4f_props)) {
+    __android_log_print(ANDROID_LOG_ERROR, LOG_COMPONENT, "Could not define properties on Vector4f\n");
+  }
+	JS_SetPrivate(self, (void *)vec);
 	return self;
 }
 
 OVR::Vector4f* GetVector4f(JS::HandleObject obj) {
-	OVR::Vector4f* vector4f = (OVR::Vector4f*)JS_GetPrivate(obj);
-	return vector4f;
+	return (OVR::Vector4f*)JS_GetPrivate(obj);
 }
 
 bool CoreVector4f_constructor(JSContext* cx, unsigned argc, JS::Value *vp) {
@@ -61,90 +151,11 @@ bool CoreVector4f_constructor(JSContext* cx, unsigned argc, JS::Value *vp) {
 		return false;
 	}
 
-	OVR::Vector4f* vector4f = new OVR::Vector4f(x, y, z, w);
-
 	// Go ahead and create our self object
-	JS::RootedObject self(cx, NewCoreVector4f(cx, vector4f));
+	JS::RootedObject self(cx, NewCoreVector4f(cx, new OVR::Vector4f(x, y, z, w)));
 
 	// Return our self object
 	args.rval().set(JS::ObjectOrNullValue(self));
-	return true;
-}
-
-bool _setVector4fVertex(JSContext* cx, JS::MutableHandleValue vp, JS::RootedString* propertyName, const char* propName, float* out) {
-	bool match;
-	// TODO: Can we do this in a more efficient way than string scanning?! Maybe interned strings.
-	//       But first measure overhead, maybe this is fast enough.
-	if (!JS_StringEqualsAscii(cx, *propertyName, propName, &match)) {
-		JS_ReportError(cx, "Could not compare strings");
-		return false;
-	}
-	if (match) {
-		if (!vp.isNumber()) {
-			JS_ReportError(cx, "Vector4f arguments must all be numbers");
-			return false;
-		}
-		double d;
-		if (!JS::ToNumber(cx, vp, &d)) {
-			JS_ReportError(cx, "Could not convert argument to double");
-			return false;
-		}
-		*out = (float)d;
-	}
-	return true;
-}
-
-bool _getVector4fVertex(JSContext* cx, JS::MutableHandleValue vp, JS::RootedString* propertyName, const char* propName, float* num) {
-	bool match;
-	// TODO: Can we do this in a more efficient way than string scanning?! Maybe interned strings.
-	//       But first measure overhead, maybe this is fast enough.
-	if (!JS_StringEqualsAscii(cx, *propertyName, propName, &match)) {
-		JS_ReportError(cx, "Could not compare strings");
-		return false;
-	}
-	if (match) {
-		vp.setNumber((double)*num);
-	}
-	return true;
-}
-
-bool CoreVector4f_setProperty(JSContext* cx, JS::HandleObject obj, JS::HandleId id, JS::MutableHandleValue vp, JS::ObjectOpResult& result) {
-	if (JSID_IS_STRING(id)) {
-		JS::RootedString propertyName(cx, JSID_TO_STRING(id));
-		OVR::Vector4f* vector4f = GetVector4f(obj);
-		if (!_setVector4fVertex(cx, vp, &propertyName, "x", &vector4f->x)) {
-			return false;
-		}
-		if (!_setVector4fVertex(cx, vp, &propertyName, "y", &vector4f->y)) {
-			return false;
-		}
-		if (!_setVector4fVertex(cx, vp, &propertyName, "z", &vector4f->z)) {
-			return false;
-		}
-		if (!_setVector4fVertex(cx, vp, &propertyName, "w", &vector4f->w)) {
-			return false;
-		}
-	}
-	return true;
-}
-
-bool CoreVector4f_getProperty(JSContext* cx, JS::HandleObject obj, JS::HandleId id, JS::MutableHandleValue vp) {
-	if (JSID_IS_STRING(id)) {
-		JS::RootedString propertyName(cx, JSID_TO_STRING(id));
-		OVR::Vector4f* vector4f = GetVector4f(obj);
-		if (!_getVector4fVertex(cx, vp, &propertyName, "x", &vector4f->x)) {
-			return false;
-		}
-		if (!_getVector4fVertex(cx, vp, &propertyName, "y", &vector4f->y)) {
-			return false;
-		}
-		if (!_getVector4fVertex(cx, vp, &propertyName, "z", &vector4f->z)) {
-			return false;
-		}
-		if (!_getVector4fVertex(cx, vp, &propertyName, "w", &vector4f->w)) {
-			return false;
-		}
-	}
 	return true;
 }
 
--- Src/CoreVector4f.h
+++ Src/CoreVector4f.h
@@ -4,13 +4,8 @@
 #include "BaseInclude.h"
 
 void SetupCoreVector4f(JSContext* cx, JS::RootedObject *global, JS::RootedObject *core);
-JSObject* NewCoreVector4f(JSContext* cx, OVR::Vector4f* vector4f);
+JSObject* NewCoreVector4f(JSContext* cx, OVR::Vector4f* vec);
 OVR::Vector4f* GetVector4f(JS::HandleObject obj);
-
-//
-
-bool CoreVector4f_getProperty(JSContext* cx, JS::HandleObject obj, JS::HandleId id, JS::MutableHandleValue vp);
-bool CoreVector4f_setProperty(JSContext* cx, JS::HandleObject obj, JS::HandleId id, JS::MutableHandleValue vp, JS::ObjectOpResult& result);
 void CoreVector4f_finalize(JSFreeOp *fop, JSObject *obj);
 const JSClass* CoreVector4f_class();
 
